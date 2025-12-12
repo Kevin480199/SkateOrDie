1b7249c39b87a11335f9c97d3d0531f156520657
Kevin Lu <ke28lu@gmail.com>
2025-12-12 11:10:19 +0100

    Bug fix with hover menu

--- SkateOrDie - FrontEnd/src/components/MenuBar.jsx
+++ SkateOrDie - FrontEnd/src/components/MenuBar.jsx
@@ -6,17 +6,22 @@ export default function Menubar(){
             <nav className="bg-gray-800 p-4">
             <ul className="flex space-x-4 text-white">
                 <li className="relative group">
-                <a href="#" className="px-4 py-2 block hover:bg-gray-700 rounded">HERR</a>
-                
-                <ul className="absolute left-0 mt-2 w-40 bg-gray-700 rounded shadow-lg opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-opacity duration-300">
+                    <a href="#" className="px-4 py-2 block hover:bg-gray-700 rounded">HERR</a>
+                    
+                    <ul className="absolute left-0 top-full w-40 bg-gray-700 rounded shadow-lg opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-opacity duration-300 z-10">
+                        <li><a href="#" className="block px-4 py-2 hover:bg-gray-600 rounded">Kläder</a></li>
+                        <li><a href="#" className="block px-4 py-2 hover:bg-gray-600 rounded">Accessoarer</a></li>
+                        <li><a href="#" className="block px-4 py-2 hover:bg-gray-600 rounded">Skor</a></li>
+                    </ul>
+                </li>
+
+                <li className="relative group">
+                    <a href="#" className="px-4 py-2 block hover:bg-gray-700 rounded">DAM</a>
+                <ul className="absolute left-0 top-full w-40 bg-gray-700 rounded shadow-lg opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-opacity duration-300 z-10">
                     <li><a href="#" className="block px-4 py-2 hover:bg-gray-600 rounded">Kläder</a></li>
                     <li><a href="#" className="block px-4 py-2 hover:bg-gray-600 rounded">Accessoarer</a></li>
                     <li><a href="#" className="block px-4 py-2 hover:bg-gray-600 rounded">Skor</a></li>
-                </ul>
-                </li>
-
-                <li><a href="#" className="px-4 py-2 block hover:bg-gray-700 rounded">DAM</a></li>
-                <li><a href="#" className="px-4 py-2 block hover:bg-gray-700 rounded">BARN</a></li>
+                </ul></li>
             </ul>
             </nav>
         </div>
